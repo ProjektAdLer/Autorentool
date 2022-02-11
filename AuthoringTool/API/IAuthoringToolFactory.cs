13d9c7cd79be26f32c63cdf70386794659ab7224
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2022-02-11 14:59:21 +0100

    pull up CreateAuthoringToolConfiguration to interface

--- AuthoringTool/API/IAuthoringToolFactory.cs
+++ AuthoringTool/API/IAuthoringToolFactory.cs
@@ -1,8 +1,10 @@
 ﻿using AuthoringTool.API.Configuration;
+using log4net;
 
 namespace AuthoringTool.API;
 
 public interface IAuthoringToolFactory  
 {
     IAuthoringTool CreateAuthoringTool(IAuthoringToolConfiguration config);
+    IAuthoringToolConfiguration CreateAuthoringToolConfiguration(ILog logger);
 }
