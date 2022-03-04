6b139677b049ac599f1060bda44cd9f9a91a2dc3
DimitriB01 <d.bigler1996@gmx.de>
2022-03-04 11:05:25 +0100

    constructor presentationlogic unittest, moodle files template added

--- AuthoringToolTest/PresentationLogic/API/PresentationLogicUt.cs
+++ AuthoringToolTest/PresentationLogic/API/PresentationLogicUt.cs
@@ -1,9 +1,35 @@
-﻿using NUnit.Framework;
+﻿using AuthoringTool.API.Configuration;
+using AuthoringTool.BusinessLogic.API;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace AuthoringToolTest.PresentationLogic.API;
 
 [TestFixture]
 public class PresentationLogicUt
 {
+    [Test]
+    public void PresentationLogic_Standard_AllPropertiesInitialized()
+    {
+        //Arrange
+        var mockConfiguration = Substitute.For<IAuthoringToolConfiguration>();
+        var mockBusinessLogic = Substitute.For<IBusinessLogic>();
+        
+        //Act
+        var systemUnderTest = CreateStandardPresentationLogic(mockConfiguration, mockBusinessLogic);
+        
+        //Assert
+        Assert.That(systemUnderTest.Configuration, Is.EqualTo(mockConfiguration));
+        Assert.That(systemUnderTest.BusinessLogic, Is.EqualTo(mockBusinessLogic));
+        
+    }
     
+    
+
+    private static AuthoringTool.PresentationLogic.API.PresentationLogic CreateStandardPresentationLogic(IAuthoringToolConfiguration fakeConfiguration=null, IBusinessLogic fakeBusinessLogic=null)
+    {
+        fakeConfiguration ??= Substitute.For<IAuthoringToolConfiguration>();
+        fakeBusinessLogic ??= Substitute.For<IBusinessLogic>();
+        return new AuthoringTool.PresentationLogic.API.PresentationLogic(fakeConfiguration, fakeBusinessLogic);
+    }
 }
