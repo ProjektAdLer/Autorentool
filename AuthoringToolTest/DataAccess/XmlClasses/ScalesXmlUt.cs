b78e51f613510d9d8b2db183f4bdae039c88b7d3
DimitriB01 <d.bigler1996@gmx.de>
2022-03-28 19:06:21 +0200

    moved classes out of MoodleBackupXmlUt.cs

---
+++ AuthoringToolTest/DataAccess/XmlClasses/ScalesXmlUt.cs
@@ -0,0 +1,21 @@
+﻿using AuthoringTool.DataAccess.XmlClasses;
+using NUnit.Framework;
+
+namespace AuthoringToolTest.DataAccess.XmlClasses;
+
+[TestFixture]
+public class ScalesXmlUt
+{
+    [Test]
+    public void ScalesXmlScalesDefinition_SetParameters_ObjectsAreEqual()
+    {
+        //Arrange
+        var scalesScalesDefinition = new ScalesXmlScalesDefinition();
+        
+        //Act
+        scalesScalesDefinition.SetParameters();
+        
+        //Assert
+        Assert.That(scalesScalesDefinition, Is.EqualTo(scalesScalesDefinition));
+    }
+}
