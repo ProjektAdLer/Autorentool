7c44b258aae2dd0c6e71bf12f20603150cb9bcb2
DimitriB01 <d.bigler1996@gmx.de>
2022-04-07 12:00:47 +0200

    files.Serialize test

--- AuthoringToolTest/DataAccess/XmlClasses/Entities/FilesXmlUt.cs
+++ AuthoringToolTest/DataAccess/XmlClasses/Entities/FilesXmlUt.cs
@@ -1,4 +1,8 @@
-﻿using AuthoringTool.DataAccess.XmlClasses;
+﻿using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using System.Xml.Serialization;
+using AuthoringTool.DataAccess.WorldExport;
+using AuthoringTool.DataAccess.XmlClasses;
 using NUnit.Framework;
 
 namespace AuthoringToolTest.DataAccess.XmlClasses.Entities;
@@ -19,6 +23,24 @@ public class FilesXmlUt
         
         //Assert
         Assert.That(filesFiles, Is.EqualTo(filesFiles));
+    }
+    
+    [Test]
+    public void FilesXmlFiles_Serialize_XmlFileWritten()
+    {
+        //Arrange 
+        var mockFileSystem = new MockFileSystem();
+        var backupFileGen = new BackupFileGenerator(mockFileSystem);
+        backupFileGen.CreateBackupFolders();
+        var filesFiles = new FilesXmlFiles();
+        filesFiles.SetParameters();
+
+        //Act
+        XmlSerializeFileSystemProvider.FileSystem = mockFileSystem;
+        filesFiles.Serialize();
+        
+        //Assert
+        Assert.That(mockFileSystem.FileExists("C:\\XMLFilesForExport\\files.xml"), Is.True);
     }
     
     
