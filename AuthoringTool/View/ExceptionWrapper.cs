4259de630c11a7beae5ead1d2a7e473e794e86ab
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2022-04-13 21:30:52 +0200

    Implemented ExceptionWrapper type with pretty print support and Error dialog in WorkspaceView for save and load

---
+++ AuthoringTool/View/ExceptionWrapper.cs
@@ -0,0 +1,50 @@
+using System.Text;
+
+namespace AuthoringTool.View;
+
+public class ExceptionWrapper
+{
+    public string CallSite { get; }
+    public Exception? Exception { get; }
+    
+    public ExceptionWrapper(string callSite)
+    {
+        CallSite = callSite;
+    }
+
+    public ExceptionWrapper(string callSite, Exception exception)
+    {
+        CallSite = callSite;
+        Exception = exception;
+    }
+
+    public override string ToString()
+    {
+        return PrettyPrint();
+    }
+
+    public string PrettyPrint()
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append($"Exception encountered at {CallSite}");
+        if (Exception is null)
+        {
+            stringBuilder.Append('.');
+            return stringBuilder.ToString();
+        }
+
+        stringBuilder.Append(":\n");
+        stringBuilder.Append("Exception: \n");
+        var exception = Exception;
+        while (exception != null)
+        {
+            stringBuilder.Append(exception.Message);
+            if (exception.InnerException != null)
+                stringBuilder.Append("\nInner Exception:\n");
+            exception = exception.InnerException;
+        }
+
+        return stringBuilder.ToString();
+    }
+
+}
