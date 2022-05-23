afdd59459fe02d5030612e37d0e1f955456c5656
DimitriB01 <d.bigler1996@gmx.de>
2022-05-23 09:07:44 +0200

    Create File at ExportFiles destination

--- AuthoringTool/DataAccess/DSL/CreateDSL.cs
+++ AuthoringTool/DataAccess/DSL/CreateDSL.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text.Json;
+using AuthoringTool.DataAccess.WorldExport;
 using AuthoringTool.Entities;
+using Microsoft.VisualBasic;
 
 namespace AuthoringTool.DataAccess.DSL;
 
@@ -8,7 +10,8 @@ public class CreateDSL : ICreateDSL
     
     private List<LearningElement> listLearningElements;
     private List<LearningSpace> listLearningSpaces;
-    
+    private List<int> listLearningSpaceContent;
+
 
     public void WriteLearningWorld(LearningWorld learningWorld)
     {
@@ -27,9 +30,56 @@ public class CreateDSL : ICreateDSL
             learningElements = new List<LearningElementJson>(),
         };
         
-        //Create Learning Elements & Fill into Learning World
+        
+        // Create Learning Spaces & Fill into Learning World
+        // Search for Learning Elements in Spaces and add to listLearningElements
         listLearningElements = new List<LearningElement>();
-        listLearningElements = learningWorld.LearningElements;
+        listLearningSpaces = new List<LearningSpace>();
+        listLearningSpaces = learningWorld.LearningSpaces;
+        listLearningSpaceContent = new List<int>();
+        int learningSpaceId = 0;
+
+        foreach (var learningSpace in listLearningSpaces)
+        {
+            IdentifierJson learningSpaceIdentifier = new IdentifierJson()
+            {
+                type = "name",
+                value = learningSpace.Name
+            };
+
+            var somth = learningSpace.LearningElements;
+
+            //Which Learning Elements are in each Space
+            foreach (var element in learningSpace.LearningElements)
+            {
+                listLearningElements.Add(element);
+                int elementIndex = listLearningElements.IndexOf(element);
+                listLearningSpaceContent.Add(elementIndex);
+            }
+
+            LearningSpaceJson learningSpaceJson = new LearningSpaceJson()
+            {
+                spaceId = learningSpaceId,
+                identifier = learningSpaceIdentifier,
+                learningSpaceName = learningSpace.Name,
+                learningSpaceContent = listLearningSpaceContent
+            };
+            
+            learningSpaceId++;
+            
+            // Add Learning Space to Learning World
+            learningWorldJson.learningSpaces.Add(learningSpaceJson);
+        }
+        
+        
+        //Create Learning Elements & Fill into Learning World
+        // This Part only adds learning Elements which are in the Learning World (not into Spaces)
+        // But it creates all ELements in the List (this includes Spaces & learning World)
+
+        foreach (var element in learningWorld.LearningElements)
+        {
+            listLearningElements.Add(element);
+        }
         int learningElementId = 0;
 
         foreach (var learningElement in listLearningElements)
@@ -37,9 +87,8 @@ public class CreateDSL : ICreateDSL
             IdentifierJson learningElementIdentifier = new IdentifierJson()
             {
                 type = "FileName",
-                value = learningElement.Name,
+                value = learningElement.Name
             };
-            learningElementId++;
 
             LearningElementJson learningElementJson = new LearningElementJson()
             {
@@ -48,34 +97,13 @@ public class CreateDSL : ICreateDSL
                 elementType = learningElement.ContentType,
             };
             
+            learningElementId++;
+            
             //Add Learning Elements to Learning World
             learningWorldJson.learningElements.Add(learningElementJson);
         }
         
-        // Create Learning Spaces & Fill into Learning World
-        listLearningSpaces = new List<LearningSpace>();
-        listLearningSpaces = learningWorld.LearningSpaces;
-        int learningSpaceId = 0;
-
-        foreach (var learningSpace in listLearningSpaces)
-        {
-            IdentifierJson learningSpaceIdentifier = new IdentifierJson()
-            {
-                type = "name",
-                value = learningSpace.Name,
-            };
-            learningSpaceId++;
-
-            LearningSpaceJson learningSpaceJson = new LearningSpaceJson()
-            {
-                spaceId = learningSpaceId,
-                identifier = learningSpaceIdentifier,
-                learningSpaceName = learningSpace.Name,
-            };
-            
-            // Add Learning Space to Learning World
-            learningWorldJson.learningSpaces.Add(learningSpaceJson);
-        }
+        
         
         
 
@@ -87,7 +115,11 @@ public class CreateDSL : ICreateDSL
         
         var options = new JsonSerializerOptions { WriteIndented = true };
         var jsonFile = JsonSerializer.Serialize(rootJson,options);
-
+        
+        //Create Filepath
+        BackupFileGenerator createFolders = new BackupFileGenerator();
+        createFolders.CreateBackupFolders();
+        File.WriteAllText("XMLFilesForExport/DSL_Document.json", jsonFile);
         Console.WriteLine(jsonFile);
 
 
