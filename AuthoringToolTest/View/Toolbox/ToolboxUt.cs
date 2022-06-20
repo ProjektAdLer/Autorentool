04198e50a3db33e6e91accecb1804d7f43589e4a
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2022-06-20 21:47:10 +0200

    Toolbox tweaks and unit tests

---
+++ AuthoringToolTest/View/Toolbox/ToolboxUt.cs
@@ -0,0 +1,86 @@
+using AuthoringTool.PresentationLogic;
+using AuthoringTool.PresentationLogic.Toolbox;
+using AuthoringTool.View.Toolbox;
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using NUnit.Framework;
+using TestContext = Bunit.TestContext;
+
+namespace AuthoringToolTest.View.Toolbox;
+
+[TestFixture]
+public class ToolboxUt
+{
+    private TestContext ctx = null!;
+    private IAbstractToolboxRenderFragmentFactory fragmentFactory = null!;
+    private IToolboxEntriesProvider entriesProvider = null!;
+    private IToolboxResultFilter resultFilter = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        ctx = new TestContext();
+        fragmentFactory = Substitute.For<IAbstractToolboxRenderFragmentFactory>();
+        entriesProvider = Substitute.For<IToolboxEntriesProvider>();
+        resultFilter = Substitute.For<IToolboxResultFilter>();
+        ctx.Services.AddLogging();
+        ctx.Services.AddSingleton(fragmentFactory);
+        ctx.Services.AddSingleton(entriesProvider);
+        ctx.Services.AddSingleton(resultFilter);
+    }
+    
+    [Test]
+    public void Toolbox_Constructor_SetsParametersAndInjectsServices()
+    {
+        var systemUnderTest = GetToolboxForTest();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(systemUnderTest.Instance.Logger, Is.Not.Null);
+            Assert.That(systemUnderTest.Instance.RenderFragmentFactory, Is.EqualTo(fragmentFactory));
+            Assert.That(systemUnderTest.Instance.EntriesProvider, Is.EqualTo(entriesProvider));
+            Assert.That(systemUnderTest.Instance.ResultFilter, Is.EqualTo(resultFilter));
+        });
+    }
+
+    [Test]
+    public void Toolbox_SearchBox_SetsSearchTerm()
+    {
+        const string input = "foobar this is my search term";
+        
+        var systemUnderTest = GetToolboxForTest();
+        
+        Assert.That(string.IsNullOrEmpty(systemUnderTest.Instance.SearchTerm));
+        
+        systemUnderTest.Find("input").Input(input);
+        
+        Assert.That(systemUnderTest.Instance.SearchTerm, Is.EqualTo(input));
+    }
+
+    [Test]
+    public void Toolbox_SearchBox_EntriesFilteredWithSearchTerm()
+    {
+        var systemUnderTest = GetToolboxForTest();
+
+        entriesProvider.Entries.Received();
+
+        var entries = new IDisplayableLearningObject[] { null!, null!, null! };
+        entriesProvider.Entries.Returns(entries);
+        
+        const string input = "foobar this is my search term";
+        systemUnderTest.Find("input").Input(input);
+
+        resultFilter.Received().FilterCollection(entries, input);
+        
+        var filteredEntries = new IDisplayableLearningObject[] { null!, null! };
+        resultFilter.FilterCollection(entries, input).Returns(filteredEntries);
+        
+        Assert.That(systemUnderTest.Instance.FilteredEntries, Is.EqualTo(filteredEntries));
+    }
+    
+    private IRenderedComponent<AuthoringTool.View.Toolbox.Toolbox> GetToolboxForTest()
+    {
+        return ctx.RenderComponent<AuthoringTool.View.Toolbox.Toolbox>();
+    }
+}
