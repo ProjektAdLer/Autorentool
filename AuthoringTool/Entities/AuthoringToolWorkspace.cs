490d3e075978edbb0be97901bdc23e8fb55de2c3
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2022-08-18 19:50:50 +0200

    AuthoringToolWorkspace entity + memento pattern

---
+++ AuthoringTool/Entities/AuthoringToolWorkspace.cs
@@ -0,0 +1,37 @@
+namespace AuthoringTool.Entities;
+
+public class AuthoringToolWorkspace : IOriginator
+{
+    public AuthoringToolWorkspace(LearningWorld? selectedLearningWorld, List<LearningWorld> learningWorlds)
+    {
+        SelectedLearningWorld = selectedLearningWorld;
+        LearningWorlds = learningWorlds;
+    }
+    public LearningWorld? SelectedLearningWorld { get; set; }
+    public List<LearningWorld> LearningWorlds { get; set; }
+    
+    public IMemento GetMemento()
+    {
+        return new AuthoringToolWorkspaceMemento(this);
+    }
+
+    public void RestoreMemento(IMemento memento)
+    {
+        if (memento is not AuthoringToolWorkspaceMemento workspaceMemento)
+            throw new ArgumentException("incorrect IMemento implementation", nameof(memento));
+        SelectedLearningWorld = workspaceMemento.SelectedLearningWorld;
+        LearningWorlds = workspaceMemento.LearningWorlds;
+    }
+
+    private record AuthoringToolWorkspaceMemento : IMemento
+    {
+        internal AuthoringToolWorkspaceMemento(AuthoringToolWorkspace workspace)
+        {
+            SelectedLearningWorld = workspace.SelectedLearningWorld;
+            LearningWorlds = workspace.LearningWorlds.ToList();
+        }
+
+        internal LearningWorld? SelectedLearningWorld { get; }
+        internal List<LearningWorld> LearningWorlds { get; }
+    }
+}
