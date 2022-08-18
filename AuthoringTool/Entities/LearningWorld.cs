ac017b99d27c93482fa2cb8bf7f427e7bf3e66bd
Andreas Weishaupt <andreas-93@web.de>
2022-08-18 14:31:02 +0200

    Added get and restore memento methods in LearningWorld.cs

--- AuthoringTool/Entities/LearningWorld.cs
+++ AuthoringTool/Entities/LearningWorld.cs
@@ -1,6 +1,6 @@
 ﻿namespace AuthoringTool.Entities;
 
-public class LearningWorld : ILearningWorld
+public class LearningWorld : ILearningWorld, IOriginator
 {
     internal LearningWorld(string name, string shortname, string authors, string language, string description,
         string goals, List<LearningElement>? learningElements = null,
@@ -17,11 +17,59 @@ public class LearningWorld : ILearningWorld
     }
 
     public List<LearningElement> LearningElements { get; set; }
-    public List<LearningSpace>? LearningSpaces { get; set; }
+    public List<LearningSpace> LearningSpaces { get; set; }
     public string Name { get; set; }
     public string Shortname { get; set; }
     public string Authors { get; set; }
     public string Language { get; set; }
     public string Description { get; set; }
     public string Goals { get; set; }
+
+    public IMemento GetMemento()
+    {
+        return new LearningWorldMemento(Name, Shortname, Authors, Language, Description, Goals, LearningElements,
+            LearningSpaces);
+    }
+
+    public void RestoreMemento(IMemento memento)
+    {
+        if (memento is not LearningWorldMemento learningWorldMemento)
+        {
+            throw new ArgumentException("Incorrect IMemento implementation", nameof(memento));
+        }
+        Name = learningWorldMemento.Name;
+        Shortname = learningWorldMemento.Shortname;
+        Authors = learningWorldMemento.Authors;
+        Language = learningWorldMemento.Language;
+        Description = learningWorldMemento.Description;
+        Goals = learningWorldMemento.Goals;
+        LearningElements = learningWorldMemento.LearningElements;
+        LearningSpaces = learningWorldMemento.LearningSpaces;
+    }
+
+    private record LearningWorldMemento : IMemento
+    {
+        internal LearningWorldMemento(string name, string shortname, string authors, string language,
+            string description,
+            string goals, List<LearningElement> learningElements, List<LearningSpace> learningSpaces)
+        {
+            Name = name;
+            Shortname = shortname;
+            Authors = authors;
+            Language = language;
+            Description = description;
+            Goals = goals;
+            LearningElements = learningElements.ToList();
+            LearningSpaces = learningSpaces.ToList();
+        }
+
+        internal List<LearningElement> LearningElements { get; }
+        internal List<LearningSpace> LearningSpaces { get;  }
+        internal string Name { get; }
+        internal string Shortname { get; }
+        internal string Authors { get; }
+        internal string Language { get; }
+        internal string Description { get; }
+        internal string Goals { get; }
+    }
 }
--- AuthoringToolTest/Entities/LearningWorldUt.cs
+++ AuthoringToolTest/Entities/LearningWorldUt.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using AuthoringTool.Entities;
 using AuthoringTool.PresentationLogic.LearningElement;
@@ -11,33 +12,141 @@ public class LearningWorldUt
     [Test]
     public void LearningWorld_Constructor_InitializesAllProperties()
     {
-        var Name = "asdf";
-        var Shortname = "jkl;";
-        var Authors = "ben and jerry";
-        var Language = "german";
-        var Description = "very cool element";
-        var Goals = "learn very many things";
+        const string name = "asdf";
+        const string shortname = "jkl;";
+        const string authors = "ben and jerry";
+        const string language = "german";
+        const string description = "very cool element";
+        const string goals = "learn very many things";
         var content1 = new LearningContent("a", "b", new byte[]{0x01,0x02});
         var content2 = new LearningContent("w", "e", new byte[]{0x02,0x01});
         var ele1 = new LearningElement("a", "b", "e",content1, "pupup", "g","h",LearningElementDifficultyEnum.Easy, 17, 23);
         var ele2 = new LearningElement("z", "zz", "zzz", content2,"baba", "z","zz",LearningElementDifficultyEnum.Medium, 444, double.MaxValue);
-        var LearningElements = new List<LearningElement> { ele1, ele2 };
+        var learningElements = new List<LearningElement> { ele1, ele2 };
         var space1 = new LearningSpace("ff", "ff", "ff", "ff", "ff");
-        var LearningSpaces = new List<LearningSpace> { space1 };
+        var learningSpaces = new List<LearningSpace> { space1 };
 
-        var systemUnderTest = new LearningWorld(Name, Shortname, Authors, Language, Description, Goals,
-            LearningElements, LearningSpaces);
+        var systemUnderTest = new LearningWorld(name, shortname, authors, language, description, goals,
+            learningElements, learningSpaces);
         
         Assert.Multiple(() =>
         {
-            Assert.That(systemUnderTest.Name, Is.EqualTo(Name));
-            Assert.That(systemUnderTest.Shortname, Is.EqualTo(Shortname));
-            Assert.That(systemUnderTest.Authors, Is.EqualTo(Authors));
-            Assert.That(systemUnderTest.Language, Is.EqualTo(Language)); 
-            Assert.That(systemUnderTest.Description, Is.EqualTo(Description));
-            Assert.That(systemUnderTest.Goals, Is.EqualTo(Goals));
-            Assert.That(systemUnderTest.LearningElements, Is.EqualTo(LearningElements));
-            Assert.That(systemUnderTest.LearningSpaces, Is.EqualTo(LearningSpaces));
+            Assert.That(systemUnderTest.Name, Is.EqualTo(name));
+            Assert.That(systemUnderTest.Shortname, Is.EqualTo(shortname));
+            Assert.That(systemUnderTest.Authors, Is.EqualTo(authors));
+            Assert.That(systemUnderTest.Language, Is.EqualTo(language)); 
+            Assert.That(systemUnderTest.Description, Is.EqualTo(description));
+            Assert.That(systemUnderTest.Goals, Is.EqualTo(goals));
+            Assert.That(systemUnderTest.LearningElements, Is.EqualTo(learningElements));
+            Assert.That(systemUnderTest.LearningSpaces, Is.EqualTo(learningSpaces));
         });
+    }
+    
+     [Test]
+    public void GetRestoreMemento_RestoresCorrectMemento()
+    {
+        const string name = "asdf";
+        const string shortname = "jkl;";
+        const string authors = "ben and jerry";
+        const string language = "german";
+        const string description = "very cool element";
+        const string goals = "learn very many things";
+        var content1 = new LearningContent("a", "b", new byte[]{0x01,0x02});
+        var content2 = new LearningContent("w", "e", new byte[]{0x02,0x01});
+        var ele1 = new LearningElement("a", "b", "e",content1, "pupup", "g","h",LearningElementDifficultyEnum.Easy, 17, 23);
+        var ele2 = new LearningElement("z", "zz", "zzz", content2,"baba", "z","zz",LearningElementDifficultyEnum.Medium, 444, double.MaxValue);
+        var learningElements = new List<LearningElement> { ele1, ele2 };
+        var space1 = new LearningSpace("ff", "ff", "ff", "ff", "ff");
+        var learningSpaces = new List<LearningSpace> { space1 };
+        
+        var systemUnderTest = new LearningWorld(name, shortname, authors, language, description, goals, learningElements, learningSpaces);
+
+        var learningWorldMemento = systemUnderTest.GetMemento();
+        
+        var nameChanged = "qwertz";
+        var shortnameChanged = "uiop";
+        var authorsChanged = "sdfg";
+        var languageChanged = "english";
+        var descriptionChanged = "changed description";
+        var goalsChanged = "new goals";
+        var newContent2 = new LearningContent("w", "e", new byte[]{0x08,0x02});
+        var newEle2 = new LearningElement("uu", "iii", "ooo", newContent2,"lll", "kkk","fff", LearningElementDifficultyEnum.Hard, 77, 66);
+        var space2 = new LearningSpace("gg", "gg", "gg", "gg", "gg");
+
+        
+        systemUnderTest.Name = nameChanged;
+        systemUnderTest.Shortname = shortnameChanged;
+        systemUnderTest.Authors = authorsChanged;
+        systemUnderTest.Language = languageChanged;
+        systemUnderTest.Description = descriptionChanged;
+        systemUnderTest.Goals = goalsChanged;
+        systemUnderTest.LearningElements.Remove(ele2);
+        systemUnderTest.LearningElements.Add(newEle2);
+        systemUnderTest.LearningSpaces.Add(space2);
+        
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(systemUnderTest.Name, Is.EqualTo(nameChanged));
+            Assert.That(systemUnderTest.Shortname, Is.EqualTo(shortnameChanged));
+            Assert.That(systemUnderTest.Authors, Is.EqualTo(authorsChanged));
+            Assert.That(systemUnderTest.Language, Is.EqualTo(languageChanged));
+            Assert.That(systemUnderTest.Description, Is.EqualTo(descriptionChanged));
+            Assert.That(systemUnderTest.Goals, Is.EqualTo(goalsChanged));
+            Assert.That(systemUnderTest.LearningElements, Has.Count.EqualTo(2));
+            Assert.That(systemUnderTest.LearningElements[0], Is.EqualTo(ele1));
+            Assert.That(systemUnderTest.LearningElements[1], Is.EqualTo(newEle2));
+            Assert.That(systemUnderTest.LearningSpaces, Has.Count.EqualTo(2));
+            Assert.That(systemUnderTest.LearningSpaces[0], Is.EqualTo(space1));
+            Assert.That(systemUnderTest.LearningSpaces[1], Is.EqualTo(space2));
+        });
+        
+        systemUnderTest.RestoreMemento(learningWorldMemento);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(systemUnderTest.Name, Is.EqualTo(name));
+            Assert.That(systemUnderTest.Shortname, Is.EqualTo(shortname));
+            Assert.That(systemUnderTest.Authors, Is.EqualTo(authors));
+            Assert.That(systemUnderTest.Language, Is.EqualTo(language));
+            Assert.That(systemUnderTest.Description, Is.EqualTo(description));
+            Assert.That(systemUnderTest.Goals, Is.EqualTo(goals));
+            Assert.That(systemUnderTest.LearningElements, Has.Count.EqualTo(2));
+            Assert.That(systemUnderTest.LearningElements[0], Is.EqualTo(ele1));
+            Assert.That(systemUnderTest.LearningElements[1], Is.EqualTo(ele2));
+            Assert.That(systemUnderTest.LearningSpaces, Has.Count.EqualTo(1));
+            Assert.That(systemUnderTest.LearningSpaces[0], Is.EqualTo(space1));
+        });
+    }
+
+    [Test]
+    public void RestoreMemento_MementoIsNotLearningSpaceMemento_ThrowsException()
+    {
+        const string name = "asdf";
+        const string shortname = "jkl;";
+        const string authors = "ben and jerry";
+        const string language = "german";
+        const string description = "very cool element";
+        const string goals = "learn very many things";
+        var content1 = new LearningContent("a", "b", new byte[]{0x01,0x02});
+        var content2 = new LearningContent("w", "e", new byte[]{0x02,0x01});
+        var ele1 = new LearningElement("a", "b", "e",content1, "pupup", "g","h",LearningElementDifficultyEnum.Easy, 17, 23);
+        var ele2 = new LearningElement("z", "zz", "zzz", content2,"baba", "z","zz",LearningElementDifficultyEnum.Medium, 444, double.MaxValue);
+        var learningElements = new List<LearningElement> { ele1, ele2 };
+        var space1 = new LearningSpace("ff", "ff", "ff", "ff", "ff");
+        var learningSpaces = new List<LearningSpace> { space1 };
+        
+        var systemUnderTest = new LearningWorld(name, shortname, authors, language, description, goals, learningElements, learningSpaces);
+
+
+        var mementoMock = new MementoMock();
+        
+        var ex = Assert.Throws<ArgumentException>(() => systemUnderTest.RestoreMemento(mementoMock));
+        Assert.That(ex!.Message, Is.EqualTo("Incorrect IMemento implementation (Parameter 'memento')"));
+    }
+
+    private class MementoMock : IMemento
+    {
+        
     }
 }
