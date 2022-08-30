fbaa83bb31318a823529a267053f4c15d835bad7
andreasweishaupt <andreas-93@web.de>
2022-08-30 11:29:38 +0200

    Removed AddParent in BusinessLogic, now in Mapper

--- AuthoringTool/BusinessLogic/API/BusinessLogic.cs
+++ AuthoringTool/BusinessLogic/API/BusinessLogic.cs
@@ -42,9 +42,7 @@ internal class BusinessLogic : IBusinessLogic
 
     public LearningWorld LoadLearningWorld(string filepath)
     {
-        var learningWorld = Mapper.Map<LearningWorld>(DataAccess.LoadLearningWorldFromFile(filepath));
-        AddParentToLearningElements(learningWorld);
-        return learningWorld;
+        return Mapper.Map<LearningWorld>(DataAccess.LoadLearningWorldFromFile(filepath));
     }
     
     public void SaveLearningSpace(LearningSpace learningSpace, string filepath)
@@ -54,9 +52,7 @@ internal class BusinessLogic : IBusinessLogic
 
     public LearningSpace LoadLearningSpace(string filepath)
     {
-        var learningSpace = Mapper.Map<LearningSpace>(DataAccess.LoadLearningSpaceFromFile(filepath));
-        AddParentToLearningElements(learningSpace);
-        return learningSpace;
+        return Mapper.Map<LearningSpace>(DataAccess.LoadLearningSpaceFromFile(filepath));
     }
     
     public void SaveLearningElement(LearningElement learningElement, string filepath)
@@ -81,46 +77,17 @@ internal class BusinessLogic : IBusinessLogic
 
     public LearningWorld LoadLearningWorldFromStream(Stream stream)
     {
-        var learningWorld = Mapper.Map<LearningWorld>(DataAccess.LoadLearningWorldFromStream(stream));
-        AddParentToLearningElements(learningWorld);
-        return learningWorld;
+        return Mapper.Map<LearningWorld>(DataAccess.LoadLearningWorldFromStream(stream));
     }
 
     public LearningSpace LoadLearningSpaceFromStream(Stream stream)
     {
-        var learningSpace = Mapper.Map<LearningSpace>(DataAccess.LoadLearningSpaceFromStream(stream));
-        AddParentToLearningElements(learningSpace);
-        return learningSpace;
+        return Mapper.Map<LearningSpace>(DataAccess.LoadLearningSpaceFromStream(stream));
     }
 
     public LearningElement LoadLearningElementFromStream(Stream stream)
     {
         return Mapper.Map<LearningElement>(DataAccess.LoadLearningElementFromStream(stream));
-    }
-    
-    private static void AddParentToLearningElements(ILearningElementParent learningElementParent)
-    {
-        switch (learningElementParent)
-        {
-            case LearningWorld learningWorld:
-                foreach (var element in learningWorld.LearningElements)
-                {
-                    element.Parent = learningWorld;
-                }
-
-                foreach (var space in learningWorld.LearningSpaces)
-                {
-                    AddParentToLearningElements(space);
-                }
-                break;
-            
-            case LearningSpace learningSpace:
-                foreach (var element in learningSpace.LearningElements)
-                {
-                    element.Parent = learningSpace;
-                }
-                break;
-        }
     }
 
     public string FindSuitableNewSavePath(string targetFolder, string fileName, string fileEnding)
