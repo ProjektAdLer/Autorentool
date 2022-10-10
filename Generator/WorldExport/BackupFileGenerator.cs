171431864223a3456552ae4fd0c676d24adf62d4
DimitriB01 <d.bigler1996@gmx.de>
2022-10-10 15:58:35 +0200

    _filesystem on Path.RelativePath

--- Generator/WorldExport/BackupFileGenerator.cs
+++ Generator/WorldExport/BackupFileGenerator.cs
@@ -100,13 +100,13 @@ public class BackupFileGenerator : IBackupFileGenerator
         var directories = _fileSystem.Directory.EnumerateDirectories(source, "*", SearchOption.AllDirectories);
         foreach (var directory in directories)
         {
-            var relativeDirectoryPath = Path.GetRelativePath(source, directory);
+            var relativeDirectoryPath = _fileSystem.Path.GetRelativePath(source, directory);
             _fileSystem.Directory.CreateDirectory(_fileSystem.Path.Combine(targetPrefix, relativeDirectoryPath));
         }
         var files = _fileSystem.Directory.EnumerateFiles(source, "*", SearchOption.AllDirectories);
         foreach (var file in files)
         {
-            var relativeFilepath = Path.GetRelativePath(source, file);
+            var relativeFilepath = _fileSystem.Path.GetRelativePath(source, file);
             _fileSystem.File.Copy(file, _fileSystem.Path.Combine(targetPrefix, relativeFilepath));
         }
     }
