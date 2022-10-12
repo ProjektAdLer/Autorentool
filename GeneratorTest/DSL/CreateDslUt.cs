9beed794b40869348dfb9c008446f7b0bdbc42a5
DimitriB01 <d.bigler1996@gmx.de>
2022-10-12 09:31:45 +0200

    Test for unsupported LearningContentType

--- GeneratorTest/DSL/CreateDslUt.cs
+++ GeneratorTest/DSL/CreateDslUt.cs
@@ -29,12 +29,12 @@ public class CreateDslUt
         const string description = "very cool element";
         const string goals = "learn very many things";
         
-        var content1 = new LearningContentPe("FileName", "h5p", "/foo/bar.txt");
-        var content2 = new LearningContentPe("FileName", "png", "/foo/bar.txt");
-        var content3 = new LearningContentPe("FileName", "url", "/foo/bar.txt");
-        var content4 = new LearningContentPe("FileName", "txt", "/foo/foo.txt");
-        var content5 = new LearningContentPe("FileName", "pdf", "/foo/foo.txt");
-        var content6 = new LearningContentPe("FileName", "mp3", "/foo/foo.txt");
+        var content1 = new LearningContentPe("FileName", "h5p", new byte[]{0x01,0x02});
+        var content2 = new LearningContentPe("FileName", "png", new byte[]{0x01,0x02});
+        var content3 = new LearningContentPe("FileName", "url", new byte[]{0x01,0x02});
+        var content4 = new LearningContentPe("FileName", "txt", new byte[]{0x01,0x02});
+        var content5 = new LearningContentPe("FileName", "pdf", new byte[]{0x01,0x02});
+        var content6 = new LearningContentPe("FileName", "cpp", new byte[]{0x01,0x02});
 
         var ele1 = new LearningElementPe("a", "b",content1, "", "pupup", "g","h", 
             LearningElementDifficultyEnumPe.Easy, 17, 2, 23);
@@ -64,7 +64,7 @@ public class CreateDslUt
 
         var systemUnderTest = new CreateDsl(mockFileSystem, mockLogger);
         
-        var learningElementsWithContentList = new List<LearningElementPe> { ele1, ele2, ele4, ele5, ele6 };
+        var learningElementsList = new List<LearningElementPe> { ele1, ele2, ele3, ele4, ele5, ele6 };
 
         //Act
         systemUnderTest.WriteLearningWorld(learningWorld);
@@ -79,7 +79,7 @@ public class CreateDslUt
         Assert.Multiple(() =>
         {
             Assert.That(systemUnderTest.LearningWorldJson!.Identifier.Value, Is.EqualTo(name));
-            Assert.That(systemUnderTest.ContentListLearningElements, Is.EquivalentTo(learningElementsWithContentList));
+            Assert.That(systemUnderTest.ListLearningElements, Is.EquivalentTo(learningElementsList));
             Assert.That(systemUnderTest.ListLearningSpaces, Is.EquivalentTo(learningSpaces));
             Assert.That(systemUnderTest.LearningWorldJson.LearningSpaces[0].Requirements,
                 Is.EqualTo(new List<int>()));
@@ -91,4 +91,50 @@ public class CreateDslUt
             Assert.That(mockFileSystem.FileExists(pathXmlFile), Is.True);
         });
     }
+    
+    [Test]
+     public void CreateDSL_WriteLearningWorld_UnsupportedTypeExceptionThrown()
+    {
+        //Arrange
+        var mockFileSystem = new MockFileSystem();
+        var curWorkDir = mockFileSystem.Directory.GetCurrentDirectory();
+        var mockLogger = Substitute.For<ILogger<CreateDsl>>();
+        
+        const string name = "asdf";
+        const string shortname = "jkl;";
+        const string authors = "ben and jerry";
+        const string language = "german";
+        const string description = "very cool element";
+        const string goals = "learn very many things";
+        
+        var content1 = new LearningContentPe("FileName", "mp3", new byte[]{0x01,0x02});
+
+        var ele1 = new LearningElementPe("a", "b",content1, "", "pupup", "g","h", 
+            LearningElementDifficultyEnumPe.Easy, 17, 2, 23);
+
+        var space1 = new LearningSpacePe("ff", "ff", "ff", "ff", "ff", 5, 
+            null, 0, 0, new List<LearningSpacePe>(), 
+            new List<LearningSpacePe>());
+        space1.LearningElements.AddRange(new List<LearningElementPe>{ele1});
+        var learningSpaces = new List<LearningSpacePe> { space1 };
+
+        var learningWorld = new LearningWorldPe(name, shortname, authors, language, description, goals,
+             learningSpaces);
+
+        var systemUnderTest = new CreateDsl(mockFileSystem, mockLogger);
+
+        //Act
+        try
+        {
+            systemUnderTest.WriteLearningWorld(learningWorld); 
+            Assert.Fail("Learning Content Exception was not thrown");
+        }
+        catch (Exception e)
+        {
+            //Assert
+            Assert.That(e.Message, Is.EqualTo("The given LearningContent Type is not supported - in CreateDsl."));
+        }
+
+    }
+    
 }
