a7c0b8a12bb80211656a0693c6af7d206edf2a5f
MarvinHo64 <marvin.hochstetter@web.de>
2022-11-15 14:48:26 +0100

    Refactored DraggableObjectInPathWay.razor

--- Presentation/View/LearningPathWay/DraggableLearningSpace.cs
+++ Presentation/View/LearningPathWay/DraggableLearningSpace.cs
@@ -2,16 +2,13 @@ namespace Presentation.View.LearningPathWay;
 
 public class DraggableLearningSpace : DraggableObjectInPathWay
 {
-    protected override void OnInitialized()
-    {
-        OnHoveredObjectShape =
-            @"<rect transform=""translate(-3, -3)"" height=""56"" width=""106"" fill=""lightblue""></rect>
+    protected override string OnHoveredObjectShape =>  @"<rect transform=""translate(-3, -3)"" height=""56"" width=""106"" fill=""lightblue""></rect>
                                     <circle r=""6"" transform=""translate(50, 0)"" fill=""lightblue""/>";
-        ObjectShape =
-            @"<rect height=""50"" width=""100"" style=""fill:lightgreen;stroke:black;stroke-width:{0}""></rect> 
+
+    protected override string ObjectShape =>  @"<rect height=""50"" width=""100"" style=""fill:lightgreen;stroke:black;stroke-width:{0}""></rect> 
                                     <text x=""3"" y=""15"">{1}</text>";
-        DeletePathButtonShape = @"<circle r=""6"" transform=""translate(50, 0)"" fill=""white"" stroke=""gray""/>
+
+    protected override string DeletePathButtonShape => @"<circle r=""6"" transform=""translate(50, 0)"" fill=""white"" stroke=""gray""/>
                                     <polyline points=""0,0 2,0 -2,0"" transform=""translate(50,0)"" 
                                     style=""fill:none;stroke:red;stroke-width:1""/>";
-    }
 }
