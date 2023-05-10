d64189c8e864db13db8139fffc0c216b8d17ac47
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2023-05-10 15:00:49 +0200

    Basic documentation for CommandFactory interfaces

--- BusinessLogic/Commands/Element/IElementCommandFactory.cs
+++ BusinessLogic/Commands/Element/IElementCommandFactory.cs
@@ -5,42 +5,75 @@ using Shared;
 
 namespace BusinessLogic.Commands.Element;
 
+/// <summary>
+/// Factory for creating commands relating to learning elements.
+/// </summary>
 public interface IElementCommandFactory
 {
+    /// <summary>
+    /// Creates a command to create a learning element in a slot.
+    /// </summary>
     ICreateLearningElementInSlot GetCreateInSlotCommand(LearningSpace parentSpace, int slotIndex, string name,
         ILearningContent learningContent, string description, string goals,
         LearningElementDifficultyEnum difficulty, int workload, int points, double positionX, double positionY,
         Action<LearningSpace> mappingAction);
 
+    /// <summary>
+    /// Creates a command to create a learning element in a slot.
+    /// </summary>
     ICreateLearningElementInSlot GetCreateInSlotCommand(LearningSpace parentSpace, int slotIndex,
         LearningElement learningElement,
         Action<LearningSpace> mappingAction);
 
+    /// <summary>
+    /// Creates a command to create a learning element unplaced in a world.
+    /// </summary>
     ICreateUnplacedLearningElement GetCreateUnplacedCommand(LearningWorld learningWorld, string name,
         ILearningContent learningContent, string description, string goals,
         LearningElementDifficultyEnum difficulty, int workload, int points, double positionX, double positionY,
         Action<LearningWorld> mappingAction);
 
+    /// <summary>
+    /// Creates a command to delete a learning element from a space.
+    /// </summary>
     IDeleteLearningElementInSpace GetDeleteInSpaceCommand(LearningElement learningElement, LearningSpace parentSpace,
         Action<LearningSpace> mappingAction);
 
+    /// <summary>
+    /// Creates a command to delete a learning element from a world.
+    /// </summary>
     IDeleteLearningElementInWorld GetDeleteInWorldCommand(LearningElement learningElement, LearningWorld parentWorld,
         Action<LearningWorld> mappingAction);
 
+    /// <summary>
+    /// Creates a command to drag a learning element.
+    /// </summary>
     IDragLearningElement GetDragCommand(LearningElement learningElement, double oldPositionX, double oldPositionY,
         double newPositionX, double newPositionY, Action<LearningElement> mappingAction);
 
+    /// <summary>
+    /// Creates a command to edit a learning element.
+    /// </summary>
     IEditLearningElement GetEditCommand(LearningElement learningElement, LearningSpace? parentSpace, string name,
         string description, string goals, LearningElementDifficultyEnum difficulty,
         int workload, int points, ILearningContent learningContent, Action<LearningElement> mappingAction);
 
+    /// <summary>
+    /// Creates a command to load a learning element from a file.
+    /// </summary>
     ILoadLearningElement GetLoadCommand(LearningSpace parentSpace, int slotIndex, string filepath,
         IBusinessLogic businessLogic,
         Action<LearningSpace> mappingAction);
 
+    /// <summary>
+    /// Creates a command to load a learning element from a stream.
+    /// </summary>
     ILoadLearningElement GetLoadCommand(LearningSpace parentSpace, int slotIndex, Stream stream,
         IBusinessLogic businessLogic,
         Action<LearningSpace> mappingAction);
 
+    /// <summary>
+    /// Creates a command to save a learning element to a file.
+    /// </summary>
     ISaveLearningElement GetSaveCommand(IBusinessLogic businessLogic, LearningElement learningElement, string filepath);
 }
--- BusinessLogic/Commands/Space/ISpaceCommandFactory.cs
+++ BusinessLogic/Commands/Space/ISpaceCommandFactory.cs
@@ -3,27 +3,51 @@ using BusinessLogic.Entities;
 
 namespace BusinessLogic.Commands.Space;
 
+/// <summary>
+/// Factory for creating commands relating to learning spaces.
+/// </summary>
 public interface ISpaceCommandFactory
 {
+    /// <summary>
+    /// Creates a command to create a learning space.
+    /// </summary>
     ICreateLearningSpace GetCreateCommand(LearningWorld learningWorld, string name,
         string description, string goals, int requiredPoints, double positionX, double positionY, Entities.Topic? topic,
         Action<LearningWorld> mappingAction);
 
+    /// <summary>
+    /// Creates a command to create a learning space.
+    /// </summary>
     ICreateLearningSpace GetCreateCommand(LearningWorld learningWorld, LearningSpace learningSpace,
         Action<LearningWorld> mappingAction);
 
+    /// <summary>
+    /// Creates a command to delete a learning space.
+    /// </summary>
     IDeleteLearningSpace GetDeleteCommand(LearningWorld learningWorld, LearningSpace learningSpace,
         Action<LearningWorld> mappingAction);
 
+    /// <summary>
+    /// Creates a command to edit a learning space.
+    /// </summary>
     IEditLearningSpace GetEditCommand(LearningSpace learningSpace, string name,
         string description, string goals, int requiredPoints, Entities.Topic? topic,
         Action<LearningSpace> mappingAction);
 
+    /// <summary>
+    /// Creates a command to load a learning space.
+    /// </summary>
     ILoadLearningSpace GetLoadCommand(LearningWorld learningWorld, string filepath, IBusinessLogic businessLogic,
         Action<LearningWorld> mappingAction);
 
+    /// <summary>
+    /// Creates a command to load a learning space.
+    /// </summary>
     ILoadLearningSpace GetLoadCommand(LearningWorld learningWorld, Stream stream, IBusinessLogic businessLogic,
         Action<LearningWorld> mappingAction);
 
+    /// <summary>
+    /// Creates a command to save a learning space.
+    /// </summary>
     ISaveLearningSpace GetSaveCommand(IBusinessLogic businessLogic, LearningSpace learningSpace, string filepath);
 }
