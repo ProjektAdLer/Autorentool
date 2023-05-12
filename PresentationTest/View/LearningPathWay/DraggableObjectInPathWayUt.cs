276f9133d59517e0e1ed2517b31b1f76d07645ae
MarvinHo64 <marvin.hochstetter@web.de>
2023-05-12 12:57:43 +0200

    Fixed test

--- PresentationTest/View/LearningPathWay/DraggableObjectInPathWayUt.cs
+++ PresentationTest/View/LearningPathWay/DraggableObjectInPathWayUt.cs
@@ -139,7 +139,7 @@ public class DraggableObjectInPathWay
         });
         var childContent = _ctx.Render((RenderFragment)stub.Instance.Parameters[nameof(Draggable<ILearningSpaceViewModel>.ChildContent)]);
         childContent.MarkupMatches(
-            $@"<svg width=""20%"" height=""20%"" viewBox=""-100 -100 2200 2000"" preserveAspectRatio=""xMinYMin"" version=""1.1"" xmlns=""http://www.w3.org/2000/svg"" xml:space=""preserve"" style=""fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"">
+            $@"<svg width=""3rem"" height=""3rem"" viewBox=""-100 -100 2200 2000"" preserveAspectRatio=""xMinYMin"" version=""1.1"" xmlns=""http://www.w3.org/2000/svg"" xml:space=""preserve"" style=""fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"">
               <g transform=""matrix(1.12299,-0.648358,1.29672,0.74866,-322.238,-121.233)"">
                 <rect x=""-1024"" y=""1191.65"" width=""825.427"" height=""714.841"" style=""fill:rgb(185,190,198);""></rect>
                 <path d=""M-183.732,1185.22C-183.732,1181.25 -186.56,1178.8 -191.152,1178.8L-1016.58,1178.8C-1021.17,1178.8 -1026.83,1181.25 -1031.42,1185.22C-1036.01,1189.2 -1038.84,1194.1 -1038.84,1198.08L-1038.84,1912.92C-1038.84,1916.89 -1036.01,1919.34 -1031.42,1919.34L-205.994,1919.34C-201.402,1919.34 -195.744,1916.89 -191.152,1912.92C-186.56,1908.94 -183.732,1904.04 -183.732,1900.07L-183.732,1185.22ZM-213.414,1204.5L-213.414,1893.64C-213.414,1893.64 -1009.16,1893.64 -1009.16,1893.64C-1009.16,1893.64 -1009.16,1204.5 -1009.16,1204.5C-1009.16,1204.5 -213.414,1204.5 -213.414,1204.5L-213.414,1204.5Z"" style=""fill:rgb(23,45,77);""></path>
