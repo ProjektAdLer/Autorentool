49cf29c7fd67d238b52c75a8983126c687ff94e8
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2023-06-09 19:19:20 +0200

    Fixed learning content display in CreateElementForm.razor and EditElementForm.razor Fixed 3D representation not being changeable at all due to an incorrect event subscription in the forms

--- Presentation/PresentationLogic/LearningContent/IFileContentViewModel.cs
+++ Presentation/PresentationLogic/LearningContent/IFileContentViewModel.cs
@@ -2,6 +2,6 @@ namespace Presentation.PresentationLogic.LearningContent;
 
 public interface IFileContentViewModel : ILearningContentViewModel
 {
-    string Type { get; set; }
-    string Filepath { get; set; }
+    string Type { get; init; }
+    string Filepath { get; init; }
 }
