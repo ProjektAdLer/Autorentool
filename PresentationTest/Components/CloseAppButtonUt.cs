121aa9d9dd825d01798e05d0597ff6b3fbab1fc0
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2023-08-23 16:34:01 +0200

    Fixed CloseAppButtonUt.cs

--- PresentationTest/Components/CloseAppButtonUt.cs
+++ PresentationTest/Components/CloseAppButtonUt.cs
@@ -4,6 +4,7 @@ using NSubstitute;
 using NUnit.Framework;
 using Presentation.Components;
 using Presentation.PresentationLogic;
+using TestHelpers;
 
 namespace PresentationTest.Components;
 
@@ -16,6 +17,7 @@ public class CloseAppButtonUt
         _context = new Bunit.TestContext();
         _shutdownManager = Substitute.For<IShutdownManager>();
         _context.Services.AddSingleton(_shutdownManager);
+        _context.AddLocalizerForTest<CloseAppButton>();
     }
 
     private Bunit.TestContext _context;
