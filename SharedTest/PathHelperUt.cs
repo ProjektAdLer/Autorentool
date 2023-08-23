53d96eb967adc15653d795bd19d03d75532f0d5a
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2023-08-23 17:18:52 +0200

    Fixes #249

---
+++ SharedTest/PathHelperUt.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using Shared;
+
+namespace SharedTest;
+
+[TestFixture]
+public class PathHelperUt
+{
+    [Test]
+    [TestCase("test.txt", ExpectedResult = "test.txt")]
+    [TestCase("test. txt", ExpectedResult = "test.txt")]
+    [TestCase("test .txt", ExpectedResult = "test_.txt")]
+    [TestCase("test  .txt", ExpectedResult = "test__.txt")]
+    [TestCase("test . txt", ExpectedResult = "test_.txt")]
+    [TestCase("test  .  txt", ExpectedResult = "test__.txt")]
+    [TestCase("test .  txt", ExpectedResult = "test_.txt")]
+    public string TrimFileName_WithFileNameWithSpaces_ReturnsFileNameWithoutSpaces(string input)
+    {
+        return PathHelper.TrimFileName(input);
+    }
+}
