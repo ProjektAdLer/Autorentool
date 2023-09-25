69b3b6e59f3fad7260a1e040229ef0864c040415
MarvinHo64 <marvin.hochstetter@web.de>
2023-09-25 15:22:17 +0200

    Added missing property "QuestionId" in AdaptivityQuestionJson.cs

--- Generator/DSL/AdaptivityElement/AdaptivityQuestionJson.cs
+++ Generator/DSL/AdaptivityElement/AdaptivityQuestionJson.cs
@@ -2,10 +2,11 @@ namespace Generator.DSL.AdaptivityElement;
 
 public class AdaptivityQuestionJson : IAdaptivityQuestionJson
 {
-    public AdaptivityQuestionJson(string questionType, string questionUuid, int questionDifficulty,
+    public AdaptivityQuestionJson(string questionType, string questionId, string questionUuid, int questionDifficulty,
         string questionText, List<IAdaptivityRuleJson> adaptivityRules)
     {
         QuestionType = questionType;
+        QuestionId = questionId;
         QuestionUUID = questionUuid;
         QuestionDifficulty = questionDifficulty;
         QuestionText = questionText;
@@ -14,6 +15,7 @@ public class AdaptivityQuestionJson : IAdaptivityQuestionJson
 
     public string Type => JsonTypes.AdaptivityQuestionType;
     public string QuestionType { get; set; }
+    public string QuestionId { get; set; }
     public string QuestionUUID { get; set; }
     public int QuestionDifficulty { get; set; }
     public string QuestionText { get; set; }
--- Generator/DSL/AdaptivityElement/IAdaptivityQuestionJson.cs
+++ Generator/DSL/AdaptivityElement/IAdaptivityQuestionJson.cs
@@ -3,6 +3,7 @@ namespace Generator.DSL.AdaptivityElement;
 public interface IAdaptivityQuestionJson : IHasType
 {
     string QuestionType { get; set; }
+    string QuestionId { get; set; }
     string QuestionUUID { get; set; }
     int QuestionDifficulty { get; set; }
     string QuestionText { get; set; }
