ec658559fffcf193039141130b8d11b15cf32112
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2023-10-09 13:56:55 +0200

    Created interfaces for AdaptivityAction commands

---
+++ BusinessLogic/Commands/Adaptivity/Action/AdaptivityActionCommandFactory.cs
@@ -0,0 +1,5 @@
+namespace BusinessLogic.Commands.Adaptivity.Action;
+
+class AdaptivityActionCommandFactory : IAdaptivityActionCommandFactory
+{
+}
---
+++ BusinessLogic/Commands/Adaptivity/Action/CommentAction/IEditCommentAction.cs
@@ -0,0 +1,5 @@
+namespace BusinessLogic.Commands.Adaptivity.Action.CommentAction;
+
+public interface IEditCommentAction
+{
+}
