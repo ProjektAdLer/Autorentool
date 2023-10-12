c224b136e482ba76f0cdf1cc4fe03bcf87f8ae2f
Andreas Weishaupt <andreas-93@web.de>
2023-10-12 16:23:20 +0200

    - fix test for MultipleChoiceQuestionForm

--- IntegrationTest/Forms/Adaptivity/AdaptivityQuestion/MultipleChoiceQuestionFormIt.cs
+++ IntegrationTest/Forms/Adaptivity/AdaptivityQuestion/MultipleChoiceQuestionFormIt.cs
@@ -10,13 +10,14 @@ using Presentation.Components.Adaptivity.Forms.Models;
 using Presentation.Components.Forms;
 using Presentation.PresentationLogic.API;
 using Presentation.PresentationLogic.LearningContent.AdaptivityContent;
+using Presentation.PresentationLogic.LearningContent.AdaptivityContent.Question;
 using Shared.Adaptivity;
 using TestHelpers;
 
 namespace IntegrationTest.Forms.Adaptivity.AdaptivityQuestion;
 
 [TestFixture]
-public class CreateAdaptivityQuestionFormIt : MudFormTestFixture<CreateAdaptivityQuestionForm,
+public class MultipleChoiceQuestionFormIt : MudFormTestFixture<MultipleChoiceQuestionForm,
     MultipleChoiceQuestionFormModel, IMultipleChoiceQuestion>
 {
     [SetUp]
@@ -28,9 +29,11 @@ public class CreateAdaptivityQuestionFormIt : MudFormTestFixture<CreateAdaptivit
         MultipleResponseQuestionValidator =
             Substitute.For<IValidationWrapper<MultipleChoiceMultipleResponseQuestion>>();
         SingleResponseQuestionValidator = Substitute.For<IValidationWrapper<MultipleChoiceSingleResponseQuestion>>();
+        ChoiceValidator = Substitute.For<IValidationWrapper<Choice>>();
         Context.Services.AddSingleton(PresentationLogic);
         Context.Services.AddSingleton(MultipleResponseQuestionValidator);
         Context.Services.AddSingleton(SingleResponseQuestionValidator);
+        Context.Services.AddSingleton(ChoiceValidator);
     }
 
     private IPresentationLogic PresentationLogic { get; set; } = null!;
@@ -41,15 +44,19 @@ public class CreateAdaptivityQuestionFormIt : MudFormTestFixture<CreateAdaptivit
     private IValidationWrapper<MultipleChoiceSingleResponseQuestion> SingleResponseQuestionValidator { get; set; } =
         null!;
 
+    private IValidationWrapper<Choice> ChoiceValidator { get; set; } = null!;
+
     [Test]
     public void Render_InjectsDependenciesAndParameters()
     {
         var onSubmitted = EventCallback.Factory.Create(this, () => { });
         var task = ViewModelProvider.GetAdaptivityTask();
         const QuestionDifficulty difficulty = QuestionDifficulty.Medium;
+        IMultipleChoiceQuestionViewModel?
+            questionToEdit = ViewModelProvider.GetMultipleChoiceMultipleResponseQuestion();
         var dialog = Substitute.For<MudDialogInstance>();
 
-        var systemUnderTest = GetRenderedComponent(onSubmitted, task, difficulty, dialog);
+        var systemUnderTest = GetRenderedComponent(onSubmitted, task, difficulty, questionToEdit, dialog);
         Assert.Multiple(() =>
         {
             Assert.That(systemUnderTest.Instance.Localizer, Is.Not.Null);
@@ -58,25 +65,27 @@ public class CreateAdaptivityQuestionFormIt : MudFormTestFixture<CreateAdaptivit
             Assert.That(systemUnderTest.Instance.MudDialog, Is.EqualTo(dialog));
             Assert.That(systemUnderTest.Instance.Task, Is.EqualTo(task));
             Assert.That(systemUnderTest.Instance.Difficulty, Is.EqualTo(difficulty));
+            Assert.That(systemUnderTest.Instance.QuestionToEdit, Is.EqualTo(questionToEdit));
             Assert.That(systemUnderTest.Instance.OnSubmitted, Is.EqualTo(onSubmitted));
             Assert.That(systemUnderTest.Instance.DebounceInterval, Is.EqualTo(0));
         });
     }
 
-    private IRenderedComponent<CreateAdaptivityQuestionForm> GetRenderedComponent(EventCallback? onSubmitted = null,
+    private IRenderedComponent<MultipleChoiceQuestionForm> GetRenderedComponent(EventCallback? onSubmitted = null,
         IAdaptivityTaskViewModel? task = null, QuestionDifficulty difficulty = QuestionDifficulty.Easy,
-        MudDialogInstance? mudDialogInstance = null)
+        IMultipleChoiceQuestionViewModel? questionToEdit = null, MudDialogInstance? mudDialogInstance = null)
     {
         onSubmitted ??= EventCallback.Empty;
         task ??= ViewModelProvider.GetAdaptivityTask();
         mudDialogInstance ??= Substitute.For<MudDialogInstance>();
 
-        return Context.RenderComponent<CreateAdaptivityQuestionForm>(p =>
+        return Context.RenderComponent<MultipleChoiceQuestionForm>(p =>
         {
             p.Add(c => c.OnSubmitted, onSubmitted.Value);
             p.Add(c => c.DebounceInterval, 0);
             p.Add(c => c.Task, task);
             p.Add(c => c.Difficulty, difficulty);
+            p.Add(c => c.QuestionToEdit, questionToEdit);
             p.AddCascadingValue(mudDialogInstance);
         });
     }
