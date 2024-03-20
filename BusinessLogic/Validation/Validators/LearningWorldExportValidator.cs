1d63cc233eae7e8d32c097dcbccb5772376618f2
defnegoncu <goncu.defne@gmail.com>
2024-03-20 20:54:58 +0300

    LearningWorldExportValidator and its Unit Tests are added
    IsLearningWorldValid logic from HeaderBar.razor in Presentation/View is reconstructed in BusinessLogic as a Validator.

---
+++ BusinessLogic/Validation/Validators/LearningWorldExportValidator.cs
@@ -0,0 +1,172 @@
+﻿using BusinessLogic.Entities;
+using FluentValidation;
+using BusinessLogic.Entities.LearningContent.Adaptivity;
+using BusinessLogic.Entities.LearningContent.Adaptivity.Action;
+using Microsoft.Extensions.Localization;
+
+namespace BusinessLogic.Validation.Validators
+{
+    public class LearningWorldExportValidator : AbstractValidator<ILearningWorld>
+    {
+        private readonly IStringLocalizer<LearningWorldExportValidator> _localizer;
+
+        public LearningWorldExportValidator(IStringLocalizer<LearningWorldExportValidator> localizer)
+        {
+            _localizer = localizer;
+            RuleFor(world => world)
+                .Must(HaveLearningSpace())
+                .WithMessage(world => $"<li> {_localizer["ErrorString.Missing.LearningSpace.Message"]} </li>");
+
+            RuleForEach<ILearningSpace>(world => world.LearningSpaces)
+                .Must(HaveLearningElement())
+                .WithMessage((world, space) =>
+                    $"<li> {_localizer["ErrorString.Missing.LearningElements.Message", space.Name]} </li>")
+                .Must(HaveSufficientPoints())
+                .WithMessage((world, space) =>
+                    $"<li> {_localizer["ErrorString.Insufficient.Points.Message", space.Name]} </li>");
+
+
+            RuleFor(world => ListofWorldAndAdaptivityContentPairings(world))
+                .ForEach(tuple =>
+                {
+                    tuple.Must(AdaptivityContentHasTask())
+                        .WithMessage((enumWorldElement, tupleworldelement) =>
+                            $"<li> {_localizer["ErrorString.NoTasks.Message", tupleworldelement.element.Name]} </li>");
+
+                    tuple.Must(NoAdaptivityContentTaskReferencesNonexistantElement())
+                        .WithMessage((enumWorldElement, tupleworldelement) =>
+                            $"<li> {_localizer["ErrorString.TaskReferencesNonexistantElement.Message", tupleworldelement.element.Name]} </li>");
+
+                    tuple.Must(NoAdaptivityContentTaskReferencesUnplacedElement())
+                        .WithMessage((enumWorldElement, tupleworldelement) =>
+                            $"<li> {_localizer["ErrorString.TaskReferencesUnplacedElement.Message", tupleworldelement.element.Name]} </li>");
+                    tuple.Custom((tupleWorldElement, context) =>
+                        NoElementMustReferenceElementInSpaceAfterOwnSpace(tupleWorldElement, context)
+                    );
+                });
+
+        }
+
+        private void NoElementMustReferenceElementInSpaceAfterOwnSpace(
+            (ILearningWorld world, ILearningElement element) tupleWorldElement, ValidationContext<IEnumerable<(ILearningWorld world, ILearningElement element)>> context)
+        {
+            var invalidSpaceReferences =
+                ReferencesElementInSpaceAfterOwnSpace(tupleWorldElement.element, tupleWorldElement.world);
+
+            foreach (var spaceElementTuple in invalidSpaceReferences)
+            {
+                var errorMessage =
+                    $"<li> {_localizer["ErrorString.TaskReferencesElementInSpaceAfterOwnSpace.Message", tupleWorldElement.element.Name, spaceElementTuple.Space.Name, spaceElementTuple.Element.Name]} </li>";
+                context.AddFailure(errorMessage);
+            }
+        }
+
+        private static Func<(ILearningWorld world, ILearningElement element), bool> NoAdaptivityContentTaskReferencesUnplacedElement()
+        {
+            return tupleWorldElement =>
+                tupleWorldElement.element.LearningContent is IAdaptivityContent adaptivityContent &&
+                !adaptivityContent.Tasks.Any(TaskReferencesUnplacedElement(tupleWorldElement.world));
+        }
+
+        private Func<(ILearningWorld world, ILearningElement element), bool> NoAdaptivityContentTaskReferencesNonexistantElement()
+        {
+            return tupleWorldElement =>
+                tupleWorldElement.element.LearningContent is IAdaptivityContent adaptivityContent &&
+                !TaskReferencesNonexistantElement(adaptivityContent, tupleWorldElement.world);
+        }
+
+        private static Func<(ILearningWorld world, ILearningElement element), bool> AdaptivityContentHasTask()
+        {
+            return tupleWorldElement =>
+                tupleWorldElement.element.LearningContent is IAdaptivityContent adaptivityContent &&
+                adaptivityContent.Tasks.Count > 0;
+        }
+
+        private static List<(ILearningWorld world, ILearningElement element)> ListofWorldAndAdaptivityContentPairings(ILearningWorld world)
+        {
+            return world.AllLearningElements
+                .Where(ele => ele.LearningContent is IAdaptivityContent)
+                .Select(ele => (world, ele)) 
+                .ToList();
+        }
+
+
+        private static Func<ILearningSpace, bool> HaveSufficientPoints()
+        {
+            return space => space.Points >= space.RequiredPoints;
+        }
+
+        private static Func<ILearningSpace, bool> HaveLearningElement()
+        {
+            return space => space.ContainedLearningElements.Any();
+        }
+
+        private Func<ILearningWorld, bool> HaveLearningSpace()
+        {
+            return world => !HaveNoLearningSpaces(world);
+        }
+
+        private bool HaveNoLearningSpaces(ILearningWorld world)
+        {
+            return world.LearningSpaces.Count == 0;
+        }
+
+        private bool TaskReferencesNonexistantElement(IAdaptivityContent adaptivityContent, ILearningWorld world)
+        {
+            var adaptivityContentReferencedIds = GetElementIdsContentReferences(adaptivityContent);
+            return adaptivityContentReferencedIds.Any(id => world.AllLearningElements.All(ele => ele.Id != id));
+        }
+
+        public static Func<IAdaptivityTask, bool> TaskReferencesUnplacedElement(ILearningWorld world) =>
+            task => task.Questions
+                .Any(question => question.Rules
+                    .Any(rule => rule.Action is ElementReferenceAction eravm &&
+                                 world.UnplacedLearningElements.Any(unplacedEle =>
+                                     unplacedEle.Id == eravm.ElementId)));
+
+
+        /// <summary>
+        /// Checks if the given element references any elements that are placed in spaces which come AFTER the space it
+        /// itself is in, in the partial ordering of spaces.
+        /// </summary>
+        private IEnumerable<(ILearningSpace Space, ILearningElement Element)> ReferencesElementInSpaceAfterOwnSpace(
+            ILearningElement element, ILearningWorld world)
+        {
+            var ownSpace =
+                world.LearningSpaces.SingleOrDefault(space => space.ContainedLearningElements.Contains(element));
+            //element isn't placed yet, can't say anything about it
+            if (ownSpace is null) return Enumerable.Empty<(ILearningSpace, ILearningElement)>();
+
+            //find all the id's of elements that the adaptivity content of this element we are looking at references
+            var adaptivityContent = (AdaptivityContent)element.LearningContent;
+            var adaptivityContentReferencedIds = GetElementIdsContentReferences(adaptivityContent);
+
+            //create a tuple of (space,ele) for every element in every space
+            IEnumerable<(ILearningSpace Space, ILearningElement Element)> spaceElementTuples = world.LearningSpaces
+                .SelectMany(space => space.ContainedLearningElements
+                    .Select(ele => (space, ele)));
+
+            //find all the tuples that contain an element that is referenced by the adaptivity content of the element we are looking at
+            var spaceElementTuplesReferenced =
+                spaceElementTuples.Where(tup => adaptivityContentReferencedIds.Contains(tup.Element.Id));
+
+            //find all spaces that are after the space the element we are looking at is in
+            var spacesAfterOwn = LearningSpaceExtensions.GetFollowingSpacesFrom(ownSpace);
+
+            //find all tuples that contain a space that is after our own space
+            return spaceElementTuplesReferenced.Where(tup => spacesAfterOwn.Contains(tup.Space));
+        }
+
+        public static IEnumerable<Guid> GetElementIdsContentReferences(IAdaptivityContent adaptivityContent)
+        {
+            return adaptivityContent.Tasks
+                .SelectMany(task => task.Questions
+                    .SelectMany(question => question.Rules
+                        .Select(rule => rule.Action)
+                        .Where(action => action is ElementReferenceAction)
+                        .Cast<ElementReferenceAction>()
+                        .Select(action => action.ElementId)));
+        }
+
+    }
+}
---
+++ BusinessLogicTest/Validation/Validators/LearningWorldExportValidatorUt.cs
@@ -0,0 +1,176 @@
+﻿using BusinessLogic.Entities;
+using BusinessLogic.Entities.LearningContent.Adaptivity.Action;
+using BusinessLogic.Validation.Validators;
+using Microsoft.Extensions.Localization;
+using NSubstitute;
+using NSubstitute.Core;
+using NUnit.Framework;
+using Shared;
+using TestHelpers;
+namespace BusinessLogicTest.Validation.Validators.CustomValidators
+{
+    [TestFixture]
+    public class LearningWorldExportValidatorUt
+    {
+        private LearningWorldExportValidator _validator;
+        private IStringLocalizer<LearningWorldExportValidator> _stringLocalizer;
+        [SetUp]
+        public void Setup()
+        {
+            _stringLocalizer = Substitute.For<IStringLocalizer<LearningWorldExportValidator>>();
+            _validator= new LearningWorldExportValidator(_stringLocalizer);
+            _stringLocalizer[Arg.Any<string>()].Returns(ci => new LocalizedString(ci.Arg<string>(), ci.Arg<string>()));
+            _stringLocalizer[Arg.Any<string>(), Arg.Any<object[]>()].Returns(ci =>
+                new LocalizedString(ci.Arg<string>(), FormatStringLocalizerValue(ci)));
+        }
+        private static string FormatStringLocalizerValue(CallInfo ci)
+        {
+            return ci.Arg<string>() + " " + string.Join(" ", ci.Arg<object[]>().Select(obj => obj.ToString()));
+        }
+
+        [Test]
+        public void Validate_WorldHasNoLearningSpaces_ErrorMessageOutput()
+        {
+            var world = EntityProvider.GetLearningWorld();
+
+            var result = _validator.Validate(world);
+
+            Assert.IsFalse(result.IsValid);
+            var errorMessageFound = result.Errors.Any(error =>
+                error.ErrorMessage.Equals($"<li> {_stringLocalizer["ErrorString.Missing.LearningSpace.Message"]} </li>"));
+            Assert.IsTrue(errorMessageFound);
+        }
+
+        [Test]
+        public void Validate_WorldHasLearningSpacesWithoutLearningElements_ErrorMessageOutput()
+        {
+            var world = EntityProvider.GetLearningWorldWithSpace();
+            var space = world.LearningSpaces.First();
+            
+             var result = _validator.Validate(world);
+            
+             Assert.IsFalse(result.IsValid);
+             var errorMessageFound = result.Errors.Any(error => error.ErrorMessage.Equals(
+                 $"<li> {_stringLocalizer["ErrorString.Missing.LearningElements.Message", space.Name]} </li>"));
+             Assert.IsTrue(errorMessageFound);
+
+        }
+
+        [Test]
+        public void Validate_WorldHasAdaptivityContentWithNoTasks_ErrorMessageOutput()
+        {
+            var world = EntityProvider.GetLearningWorldWithSpaceWithElement();
+            var space = world.LearningSpaces.First();
+            var element = space.LearningSpaceLayout.LearningElements.First().Value;
+            var adaptivityContent =EntityProvider.GetAdaptivityContent();
+            element.LearningContent = adaptivityContent;
+            adaptivityContent.Tasks.Clear();
+
+            var result = _validator.Validate(world);
+            
+            Assert.IsFalse(result.IsValid);
+            var errorMessageFound = result.Errors.Any(error => error.ErrorMessage.Equals(
+                $"<li> {_stringLocalizer["ErrorString.NoTasks.Message", element.Name]} </li>"));
+            Assert.IsTrue(errorMessageFound);
+        }   
+
+        [Test]
+        public void Validate_LearningSpaceHasInsufficientPoints_ErrorMessageOutput()
+        {
+            var world = new LearningWorld("a", "f", "d", "e", "f", "d", "h");
+            var space = new LearningSpace("a", "f", "d", 2, Theme.Campus);
+            var element1 = new LearningElement("a", null!, "s", "e", LearningElementDifficultyEnum.Easy,
+                ElementModel.l_h5p_blackboard_1, points: 1);
+            space.LearningSpaceLayout.LearningElements.Add(0, element1);
+            world.LearningSpaces.Add(space);
+
+            var result = _validator.Validate(world);
+            
+            Assert.IsFalse(result.IsValid);
+            var errorMessageFound = result.Errors.Any(error => error.ErrorMessage.Equals(
+                $"<li> {_stringLocalizer["ErrorString.Insufficient.Points.Message", space.Name]} </li>"));
+            Assert.IsTrue(errorMessageFound);
+        }
+
+        [Test]
+        public void Validate_AdaptivityContentReferencesNonExistentElement_ErrorMessageOutput()
+        {
+            var world = EntityProvider.GetLearningWorldWithSpaceWithElement();
+            var space = world.LearningSpaces.First();
+            var element = space.LearningSpaceLayout.LearningElements.First().Value;
+            var adaptivityContent = EntityProvider.GetAdaptivityContent();
+            adaptivityContent.Tasks.First().Questions.First().Rules.First().Action =
+                new ElementReferenceAction(Guid.NewGuid(), "foobar");
+            element.LearningContent = adaptivityContent;
+
+            var result = _validator.Validate(world);
+            
+            Assert.IsFalse(result.IsValid);
+            var errorMessageFound = result.Errors.Any(error => error.ErrorMessage.Equals(
+                $"<li> {_stringLocalizer["ErrorString.TaskReferencesNonexistantElement.Message", element.Name]} </li>"));
+            Assert.IsTrue(errorMessageFound);
+        }
+
+        [Test]
+        public void Validate_AdaptivityContentReferencesUnplacedElement_ErrorMessageOutput()
+        {
+            var world = EntityProvider.GetLearningWorldWithSpaceWithElement();
+            var space = world.LearningSpaces.First();
+            var element = space.LearningSpaceLayout.LearningElements.First().Value;
+            var adaptivityContent = EntityProvider.GetAdaptivityContent();
+            var unplacedElement = EntityProvider.GetLearningElement();
+            world.UnplacedLearningElements.Add(unplacedElement);
+            adaptivityContent.Tasks.First().Questions.First().Rules.First().Action =
+                new ElementReferenceAction(unplacedElement.Id, "foobar");
+            element.LearningContent = adaptivityContent;
+
+            var result = _validator.Validate(world);
+            
+            Assert.IsFalse(result.IsValid);
+            var errorMessageFound=result.Errors.Any(error => error.ErrorMessage.Equals(
+                $"<li> {_stringLocalizer["ErrorString.TaskReferencesUnplacedElement.Message", element.Name]} </li>"));
+            Assert.IsTrue(errorMessageFound);
+        }
+
+        [Test]
+        public void Validate_AdaptivityContentReferencesElementInSpaceAfterOwnSpace_ErrorMessageOutput()
+        {
+            var world = EntityProvider.GetLearningWorldWithSpaceWithElement();
+            var space = world.LearningSpaces.First();
+            var element = space.LearningSpaceLayout.LearningElements.First().Value;
+            var adaptivityContent = EntityProvider.GetAdaptivityContent();
+            var laterElement = EntityProvider.GetLearningElement();
+            laterElement.Points = 777;
+            var laterSpace = EntityProvider.GetLearningSpace();
+            laterSpace.LearningSpaceLayout.LearningElements.Add(0, laterElement);
+            space.OutBoundObjects.Add(laterSpace);
+            laterSpace.InBoundObjects.Add(space);
+            world.LearningSpaces.Add(laterSpace);
+            adaptivityContent.Tasks.First().Questions.First().Rules.First().Action =
+                new ElementReferenceAction(laterElement.Id, "foobar");
+            element.LearningContent = adaptivityContent;
+
+            var result = _validator.Validate(world);
+            
+            Assert.IsFalse(result.IsValid);
+            var errorMessageFound=result.Errors.Any(error => error.ErrorMessage.Equals(
+                $"<li> {_stringLocalizer["ErrorString.TaskReferencesElementInSpaceAfterOwnSpace.Message", element.Name, laterSpace.Name, laterElement.Name]} </li>"));
+            Assert.IsTrue(errorMessageFound);
+        }
+
+        [Test]
+        public void Validate_AllConditionsMet_NoErrorGenerated()
+        {
+            var world = new LearningWorld("a", "f", "d", "e", "f", "d", "h");
+            var space = new LearningSpace("a", "f", "d", 1, Theme.Campus);
+            var element = new LearningElement("a", null!, "s", "e", LearningElementDifficultyEnum.Easy,
+                ElementModel.l_h5p_blackboard_1, points: 1);
+            space.LearningSpaceLayout.LearningElements.Add(0, element);
+            world.LearningSpaces.Add(space);
+
+            var result = _validator.Validate(world);
+            
+            Assert.IsTrue(result.IsValid);
+        }
+    }
+}
