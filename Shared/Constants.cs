789fd5a34c9c5f8a7f247440cb08d737751ca06d
philgei <philipp@geirhos.com>
2024-05-28 16:53:35 +0200

    prepare 2.1.7 release

--- Shared/Constants.cs
+++ Shared/Constants.cs
@@ -2,6 +2,6 @@ namespace Shared;
 
 public static class Constants
 {
-    public const string ApplicationVersion = "2.1.5";
+    public const string ApplicationVersion = "2.1.7";
     public const string AtfVersion = "2.0";
 }
