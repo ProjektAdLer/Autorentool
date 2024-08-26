e141efeae3c5f6a1d4bab13c30df95e99f18cf26
Tobias Eigler <45689354+Teigler@users.noreply.github.com>
2024-08-26 16:12:40 +0200

    Update tobi-test.js

--- AuthoringTool/wwwroot/H5pStandalone/tobi-test.js
+++ AuthoringTool/wwwroot/H5pStandalone/tobi-test.js
@@ -26,7 +26,7 @@ window.javaScriptHalloWorldFunction = function () {
     return "hallo java script world";
 }
 */
-window.testH5P = function (containerId)
+window.testH5P2 = function (containerId)
 {
 
     const el = document.getElementById("h5p-container");
@@ -39,6 +39,54 @@ window.testH5P = function (containerId)
     new H5PStandalone.H5P(el, options);
     return "testH5P called";
 }
+
+
+window.testH5P =async function (containerId)
+{
+    const fs = require('fs');
+    const el = document.getElementById("h5p-container");
+
+
+    const options = {
+        // h5pJsonPath:  './H5pStandalone/h5p-folder/1_Start/h5p.json',
+       // h5pJsonPath:  '//localhost:8086/courses/3/h5p/6c324c62-1557-4e28-a1f7-c2df227c3c8c.h5p',
+          h5pJsonPath:  '//localhost:8001/H5pStandalone/h5p-folder/1_Start',
+        frameJs: '//localhost:8001/H5pStandalone/frame.bundle.js',
+        frameCss: '//localhost:8001/H5pStandalone/styles/h5p.css',
+    }
+    await new H5PStandalone.H5P(el, options);
+
+    H5P.externalDispatcher.on("xAPI", (event) => {
+        console.log("xAPI event", event);
+    });
+
+    H5P.xAPICompletedListener = xAPICompletedListener;
+    
+
+    return "123testH5P called123";
+}
+
+// from: https://github.com/ProjektAdLer/2D_3D_AdLer/blob/main/src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningElementModal/LearningElementModalController.ts#L95
+function xAPICompletedListener(t) {
+    if ((t.getVerb() === "completed" || t.getVerb() === "answered") &&
+        !t.getVerifiedStatementValue(["context", "contextActivities", "parent"])) 
+    {
+        let n = t.getScore(),
+            r = t.getMaxScore(),
+            i = t.getVerifiedStatementValue([
+                "object",
+                "definition",
+                "extensions",
+                "http://h5p.org/x-api/h5p-local-content-id",
+            ]);
+
+        e.setFinished(i, n, r);  
+    }
+}
+
+
+
+
 
 /*
  function javaScriptHalloWorldFunction()
