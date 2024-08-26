b1f3242abfbc72652c2f7b1766fd17ab398fd379
Tobias Eigler <45689354+Teigler@users.noreply.github.com>
2024-08-26 11:48:47 +0200

    tests

--- AuthoringTool/wwwroot/H5pStandalone/tobi-test.js
+++ AuthoringTool/wwwroot/H5pStandalone/tobi-test.js
@@ -20,23 +20,24 @@ export function javaScriptHalloWorldFunction() {
 
 */
 
-
+/*
 window.javaScriptHalloWorldFunction = function () {
     console.log("hello java script world");
     return "hallo java script world";
 }
-
+*/
 window.testH5P = function (containerId)
 {
 
     const el = document.getElementById("h5p-container");
     const options = {
        // h5pJsonPath:  './H5pStandalone/h5p-folder/1_Start/h5p.json',
-        h5pJsonPath:  './H5pStandalone/h5p-folder/AbfrageDefinitionen/h5p.json',
-        frameJs: './H5pStandalone/frame.bundle.js',
-        frameCss: './H5pStandalone/styles/h5p.css',
+        h5pJsonPath:  '/localhost/H5pStandalone/h5p-folder/AbfrageDefinitionen/h5p.json',
+        frameJs: '/H5pStandalone/frame.bundle.js',
+        frameCss: '/H5pStandalone/styles/h5p.css',
     }
     new H5PStandalone.H5P(el, options);
+    return "hallo java script world";
 }
 
 /*
