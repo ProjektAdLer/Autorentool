6019cba55629476a4d283ada07e50bd5e4dcb2ae
Tobias Eigler <45689354+Teigler@users.noreply.github.com>
2024-09-17 13:09:30 +0200

    add nunit Testproject for H5pPlayer

---
+++ H5pPlayer/H5pPlayerTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace H5pPlayerTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
