fb7f9fc07e563a90c18e5b785b71aeedace76cfd
Tobias Eigler <45689354+Teigler@users.noreply.github.com>
2024-11-11 13:25:06 +0100

    Create CleanupH5pPlayerPortFactory

---
+++ H5pPlayer/H5pPlayerTest/BusinessLogic/Api/CleanupH5pPlayer/CleanupH5pPlayerPortUt.cs
@@ -0,0 +1,19 @@
+﻿using H5pPlayer.BusinessLogic.Api.CleanupH5pPlayer;
+using H5pPlayer.BusinessLogic.BusinessRules;
+
+namespace H5pPlayerTest.BusinessLogic.Api.CleanupH5pPlayer;
+
+[TestFixture]
+public class CleanupH5pPlayerPortUt
+{
+
+    [Test]
+    public void CreateCleanupH5pPlayerPort()
+    {
+        var systemUnderTest = new CleanupH5pPlayerPortFactory();
+
+        var cleanupH5pPlayer =  systemUnderTest.CreateCleanupH5pPlayerPort();
+        
+        Assert.That(cleanupH5pPlayer, Is.TypeOf<TemporaryH5PsInWwwrootManager>());
+    }
+}
