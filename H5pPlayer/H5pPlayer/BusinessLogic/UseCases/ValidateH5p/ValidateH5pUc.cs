199f9d19eea58217c8327f115ede120b8025009d
Tobias Eigler <45689354+Teigler@users.noreply.github.com>
2024-11-25 10:47:04 +0100

    Update ValidateH5pUc.cs

--- H5pPlayer/H5pPlayer/BusinessLogic/UseCases/ValidateH5p/ValidateH5pUc.cs
+++ H5pPlayer/H5pPlayer/BusinessLogic/UseCases/ValidateH5p/ValidateH5pUc.cs
@@ -5,16 +5,5 @@ namespace H5pPlayer.BusinessLogic.UseCases.ValidateH5p;
 
 public class ValidateH5pUc
 {
-    // muss nach validate:
-    [JSInvokable]
-    public static void ReceiveJsonData22222(string jsonData)
-    {
-        // JSON-Daten deserialisieren
-        var jsonObject = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonData);
-
-        Console.WriteLine($"Received JSON data from JavaScript: {jsonObject}");
-        Console.WriteLine($"Received JSON data from JavaScript: {jsonData}");
-
-        // Hier kannst du die JSON-Daten weiterverarbeiten
-    }
+   
 }
