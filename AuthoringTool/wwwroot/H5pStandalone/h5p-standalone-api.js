2da1c29504904656311959713940451112faed31
Tobias Eigler <45689354+Teigler@users.noreply.github.com>
2024-12-03 14:04:54 +0100

    experimental setup for resive json data from java script

--- AuthoringTool/wwwroot/H5pStandalone/h5p-standalone-api.js
+++ AuthoringTool/wwwroot/H5pStandalone/h5p-standalone-api.js
@@ -6,11 +6,27 @@
 // "text/html". Strict MIME type checking is enforced for
 // module scripts per HTML spec
 
+
+
 window.onload = function () {
 
     let h5pInstance = null;
-    let externalDispatcher = null;
-    let xAPICompletedListener = null;
+
+
+
+    // can throw exceptions
+    window.terminateH5pStandalone = async function () {
+        const el = document.getElementById("h5p-container");
+        resetHtmlContainerOfH5p(el);
+        localStorage.clear();
+        sessionStorage.clear();
+        H5PIntegration.contents = {};
+        resetH5pInstance();
+        window.location.reload();
+        console.log("Container completely removed and rebuilt.");
+    };
+    
+    
 
 
     // can throw exceptions
@@ -18,45 +34,51 @@ window.onload = function () {
 
         console.log("displayH5p javascript function called with path:", h5pJsonPath);
         resetH5pInstance();
-        await buildH5p(h5pJsonPath);
+        await buildH5pToDisplay(h5pJsonPath);
     }
 
+    async function buildH5pToDisplay(h5pJsonPath) {
+
+        const el = document.getElementById("h5p-container");
+
+        if (el) {
+            const options = CreateH5pOptions(h5pJsonPath);
+            try {
+                await createH5pInstanceToDisplay(el, options);
+            } catch (error) {
+                console.error("Error during instantiation of H5P:", error);
+                throw  error;
+            }
+
+        } else {
+            console.error("Can not find H5P-Container element.");
+        }
+    }
+    
+    async function createH5pInstanceToDisplay(el, options) {
+
+        h5pInstance = new H5PStandalone.H5P(el, options);
+        await h5pInstance;
+    }
+    
+    
+    
     // can throw exceptions
     window.validateH5p = async function (h5pJsonPath) {
 
         console.log("validateH5p javascript function called with path:", h5pJsonPath);
         resetH5pInstance();
-        await buildH5p(h5pJsonPath);
-        
-       // setupEventListenersAndCallbackToDotNet();
+        await buildH5pToValidate(h5pJsonPath);
     }
-         
-    // can throw exceptions
-    window.terminateH5pStandalone = async function () {
-        const el = document.getElementById("h5p-container");
-        resetHtmlContainerOfH5p(el);
-        localStorage.clear();
-        sessionStorage.clear();
-        releaseEventListeners();
-        H5PIntegration.contents = {};
-        resetH5pInstance();
-        window.location.reload();
-        console.log("Container completely removed and rebuilt.");
-    };
 
-    async function buildH5p(h5pJsonPath) {
+    async function buildH5pToValidate(h5pJsonPath) {
 
         const el = document.getElementById("h5p-container");
 
         if (el) {
-            const options = {
-                h5pJsonPath: h5pJsonPath,
-                frameJs: '//localhost:8001/H5pStandalone/frame.bundle.js',
-                frameCss: '//localhost:8001/H5pStandalone/styles/h5p.css',
-            }
-
+            const options = CreateH5pOptions(h5pJsonPath);
             try {
-                await createH5pInstance(el, options);
+                await createH5pInstanceToValidate(el,options);
             } catch (error) {
                 console.error("Error during instantiation of H5P:", error);
                 throw  error;
@@ -67,24 +89,33 @@ window.onload = function () {
         }
     }
 
-    async function createH5pInstance(el, options) {
+    async function createH5pInstanceToValidate(el, options) {
 
-        h5pInstance = new H5PStandalone.H5P(el, options);
-        await h5pInstance;
+        h5pInstance = new H5PStandalone.H5P(el, options).then(function () {
+            H5P.externalDispatcher.on("xAPI", (event) => {
+                console.log("xAPI event", event);
 
-        h5pInstance.externalDispatcher.on("xAPI", (event) => {
-            console.log("xAPI event", event);
+                // Konvertiere das JavaScript-Event in ein JSON-String
+                const jsonData = JSON.stringify(event);
 
-            // Konvertiere das JavaScript-Event in ein JSON-String
-            const jsonData = JSON.stringify(event);
-
-            // JSON-Daten an die .NET-Methode senden  
-            DotNet.invokeMethodAsync('H5pPlayer.BusinessLogic.Api.JavaScript', 'ReceiveJsonData', jsonData);
+                // JSON-Daten an die .NET-Methode senden  
+                DotNet.invokeMethodAsync('H5pPlayer', 'ReceiveJsonData', jsonData);
+            });
         });
-
-        //h5pInstance.xAPICompletedListener = xAPICompletedListener;
+        await h5pInstance;
+        // frage an Phil ist raus: warum wird der completed in 3d gebraucht
+        // h5pInstance.xAPICompletedListener = xAPICompletedListener;
     }
 
+    
+    function CreateH5pOptions(h5pJsonPath) {
+        const options = {
+            h5pJsonPath: h5pJsonPath,
+            frameJs: '//localhost:8001/H5pStandalone/frame.bundle.js',
+            frameCss: '//localhost:8001/H5pStandalone/styles/h5p.css',
+        }
+        return options;
+    }
 
     function resetH5pInstance() {
         if (h5pInstance) {
@@ -97,38 +128,6 @@ window.onload = function () {
             el.innerHTML = "";
         }
     }
-
-  
-    function setupEventListenersAndCallbackToDotNet() {
-        
-        if (h5pInstance && !externalDispatcher) {
-            
-            externalDispatcher = h5pInstance.externalDispatcher;
-
-            if (externalDispatcher) {
-                
-                externalDispatcher.on("xAPI", onXAPIEvent);
-                xAPICompletedListener = onXAPIEvent; 
-            } else {
-                console.error("External dispatcher is not available.");
-            }
-        }
-    }
-
-    function onXAPIEvent(event) {
-        console.log("xAPI event received:", event);
-        const jsonData = JSON.stringify(event);
-        DotNet.invokeMethodAsync('H5pPlayer.BusinessLogic.Api.JavaScript', 'ReceiveJsonData', jsonData);
-    }
-
-
-    function releaseEventListeners() {
-        if (externalDispatcher && xAPICompletedListener) {
-            externalDispatcher.off("xAPI", xAPICompletedListener);
-            console.log("xAPI Event listeners successfully removed.");
-        }
-        externalDispatcher = null;
-        xAPICompletedListener = null;
-    }
+   
 }
 
--- H5pPlayer/H5pPlayer/BusinessLogic/Api/JavaScript/JavaScriptAdapter.cs
+++ H5pPlayer/H5pPlayer/BusinessLogic/Api/JavaScript/JavaScriptAdapter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 using H5pPlayer.BusinessLogic.UseCases.DisplayH5p;
 using Microsoft.JSInterop;
 
@@ -92,10 +93,34 @@ namespace H5pPlayer.BusinessLogic.Api.JavaScript
             // JSON-Daten deserialisieren
             var jsonObject = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonData);
 
-            Console.WriteLine($"Received JSON data from JavaScript: {jsonObject}");
-            Console.WriteLine($"Received JSON data from JavaScript: {jsonData}");
+            // Console.WriteLine($"Received JSON data from JavaScript: {jsonObject}");
+            // Console.WriteLine($"Received JSON data from JavaScript: {jsonData}");
+    
+       
+            
+            // if verb contains completed -> Dann ist H5P abschließbar
+            try
+            {
+                // JSON parsen
+                var root = JsonNode.Parse(jsonData);
 
+                // `verb` extrahieren
+                var verbPart = root?["data"]?["statement"]?["verb"];
 
+                // Prüfung, ob die `verb`-ID "completed" enthält
+                bool isCompleted = verbPart?["id"]?.GetValue<string>() == "http://adlnet.gov/expapi/verbs/completed";
+
+                // Ergebnisse anzeigen
+                Console.WriteLine("Verb-Teil:");
+                Console.WriteLine(verbPart?.ToJsonString() ?? "Verb nicht gefunden");
+
+                Console.WriteLine("\nPrüfungen:");
+                Console.WriteLine($"Abgeschlossen (verb: completed): {isCompleted}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler beim Verarbeiten des JSON: {ex.Message}");
+            }
         }
         
 
