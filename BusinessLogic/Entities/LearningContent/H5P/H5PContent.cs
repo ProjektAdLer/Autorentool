bc1a75a5d25b8ebadc43bce6a0670c7152201659
defnegoncu <goncu.defne@gmail.com>
2025-04-01 09:58:26 +0200

    H5PContent is added as a domain object.

---
+++ BusinessLogic/Entities/LearningContent/H5P/H5PContent.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLogic.Entities.LearningContent.H5P;
+
+public class H5PContent
+{
+    public H5PContentStateEnum _state { get; set; }
+
+    H5PContent(H5PContentStateEnum state)
+    {
+        _state = state;
+    }
+}
---
+++ BusinessLogic/Entities/LearningContent/H5P/H5PContentStateEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace BusinessLogic.Entities.LearningContent.H5P;
+
+public enum H5PContentStateEnum
+{
+    NOT_VALIDATED = 0,
+    NOT_COMPLETABLE = 1,
+    COMPLETABLE = 2,
+}
