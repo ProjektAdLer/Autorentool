10919d6afa795870fdd23137e361fb6299485b04
Andreas Weishaupt <andreas-93@web.de>
2025-06-27 12:26:53 +0200

    Updated Changelog.md ump authoringTool to version 2.3.0

--- Shared/Constants.cs
+++ Shared/Constants.cs
@@ -2,7 +2,7 @@ namespace Shared;
 
 public static class Constants
 {
-    public const string ApplicationVersion = "2.2.3-rc.2";
+    public const string ApplicationVersion = "2.3.0";
     public const string AtfVersion = "2.3.0"; // This Version number can be found in the Repo of the ATF: https://github.com/ProjektAdLer/TransferFileFormat PG
     public const string PluginReleaseSetVersion = "4.0.0";
 }
